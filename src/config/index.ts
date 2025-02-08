7181a09124f8b2c6c94168d82dce23f8a572f37b
Massick <4ndyraul@gmail.com>
2025-02-08 00:23:53 -0300

    change model order

--- src/config/index.ts
+++ src/config/index.ts
@@ -4,6 +4,16 @@ export const apiKey = process.env.OPENROUTER_API_KEY;
 
 export const aiModels = [
     {
+        model: "google/gemini-2.0-pro-exp-02-05:free",
+        name: "Gemini Pro 2.0 Experimental",
+        image: true
+    },
+    {
+        model: "qwen/qwen2.5-vl-72b-instruct:free",
+        name: "Qwen2.5 VL 72B Instruct",
+        image: true
+    },
+    {
         model: "deepseek/deepseek-r1-distill-llama-70b:free",
         name: "DeepSeek: R1 Distill Llama 70B",
         image: false
@@ -18,14 +28,4 @@ export const aiModels = [
         name: "Gemini 2.0 Flash Thinking Experimental 01-21",
         image: true
     },
-    {
-        model: "google/gemini-2.0-pro-exp-02-05:free",
-        name: "Gemini Pro 2.0 Experimental",
-        image: true
-    },
-    {
-        model: "qwen/qwen2.5-vl-72b-instruct:free",
-        name: "Qwen2.5 VL 72B Instruct",
-        image: true
-    }
 ]
