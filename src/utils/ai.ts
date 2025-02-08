7e18dee11d4f3ce716d3aa46ea4a673e8f120a1e
Massick <4ndyraul@gmail.com>
2025-02-08 00:23:08 -0300

    use message history

--- src/utils/ai.ts
+++ src/utils/ai.ts
@@ -12,6 +12,11 @@ export const processCommand = async (ctx: BotContext, message: string) => {
         return;
     }
 
+    // check for history
+    if (ctx.session.history.length === 0) {
+        ctx.session.history.push(ctx.session.initialPrompt)
+    }
+
     let model = ctx.session.model.model;
     let search = message
 
@@ -25,96 +30,120 @@ export const processCommand = async (ctx: BotContext, message: string) => {
         }
     }
     const sanitizedInput = encodeURIComponent(search);
-    if (sanitizedInput.length > 2) {
-        try {
-            let content: any = sanitizedInput;
-            // handle images
-            if (ctx.msg.reply_to_message && ctx.msg.reply_to_message.photo && ctx.msg.reply_to_message.photo.length > 0) {
-                const photo = ctx.msg.reply_to_message.photo.pop()
-                if (photo) {
-                    const file = await ctx.api.getFile(photo.file_id)
-                    const fileUrl = `https://api.telegram.org/file/bot${token}/${file.file_path}`
-                    const response = await axios({
-                        method: 'get',
-                        url: fileUrl,
-                        responseType: 'arraybuffer'
-                    })
-                    const buffer = Buffer.from(response.data, 'binary').toString('base64')
-                    // check if model supports images
-                    const modelObj = aiModels.find(ai => ai.model === model)
-                    if (!modelObj || !modelObj.image) {
-                        model = aiModels.find(ai => ai.image)?.model ?? aiModels[0].model
-                    }
+    try {
+        let content: any[] = [{
+            type: "text",
+            text: sanitizedInput
+        }]
+        // handle images
+        if (ctx.msg.photo && ctx.msg.photo.length > 0) {
+            const photo = ctx.msg.photo.pop()
+            if (photo) {
+                const file = await ctx.api.getFile(photo.file_id)
+                const fileUrl = `https://api.telegram.org/file/bot${token}/${file.file_path}`
+                const response = await axios({
+                    method: 'get',
+                    url: fileUrl,
+                    responseType: 'arraybuffer'
+                })
+                const buffer = Buffer.from(response.data, 'binary').toString('base64')
+                // check if model supports images
+                const modelObj = aiModels.find(ai => ai.model === model)
+                if (!modelObj || !modelObj.image) {
+                    model = aiModels.find(ai => ai.image)?.model ?? aiModels[0].model
+                }
 
-                    content = [
-                        {
-                            type: "text",
-                            text: sanitizedInput
-                        },
-                        {
-                            type: "image_url",
-                            image_url: {
-                                url: buffer,
-                            }
-                        }
-                    ]
+                content.push({
+                    type: "image_url",
+                    image_url: {
+                        url: buffer,
+                    }
+                })
+            }
+        }
+        if (ctx.msg.reply_to_message && ctx.msg.reply_to_message.photo && ctx.msg.reply_to_message.photo.length > 0) {
+            const photo = ctx.msg.reply_to_message.photo.pop()
+            if (photo) {
+                const file = await ctx.api.getFile(photo.file_id)
+                const fileUrl = `https://api.telegram.org/file/bot${token}/${file.file_path}`
+                const response = await axios({
+                    method: 'get',
+                    url: fileUrl,
+                    responseType: 'arraybuffer'
+                })
+                const buffer = Buffer.from(response.data, 'binary').toString('base64')
+                // check if model supports images
+                const modelObj = aiModels.find(ai => ai.model === model)
+                if (!modelObj || !modelObj.image) {
+                    model = aiModels.find(ai => ai.image)?.model ?? aiModels[0].model
+                }
+
+                content.push({
+                    type: "image_url",
+                    image_url: {
+                        url: buffer,
+                    }
+                })
+            }
+        }
+        console.log("\n===========================\n")
+        console.log(content)
+
+        const res = await axios.post("https://openrouter.ai/api/v1/chat/completions",
+            {
+                model,
+                messages: [
+                    ...ctx.session.history,
+                    {
+                        role: "user",
+                        content,
+                    }
+                ]
+            },
+            {
+                headers: {
+                    "Authorization": `Bearer ${apiKey}`,
+                    "Content-Type": "application/json"
                 }
             }
+        );
 
-            const res = await axios.post("https://openrouter.ai/api/v1/chat/completions",
-                {
-                    model,
-                    messages: [
-                        {
-                            role: "user",
-                            content,
-                        }
-                    ]
-                },
-                {
-                    headers: {
-                        "Authorization": `Bearer ${apiKey}`,
-                        "Content-Type": "application/json"
-                    }
-                }
-            );
+        const aiResponse = res.data?.choices?.[0].message.content;
+        console.log("\n===========================\n")
+        console.log(aiResponse)
 
-            const aiResponse = res.data.choices[0].message.content;
+        if (!aiResponse || typeof aiResponse !== 'string') throw new Error("No response from AI (Timeout?)");
+        ctx.session.history.push({
+            role: "assistant",
+            content: aiResponse
+        })
 
-            if (!aiResponse) throw new Error("No response from AI (Timeout?)");
+        // TODO: should I parse?
+        const parsedResponse = aiResponse;
 
-            // TODO: should I parse?
-            const parsedResponse = aiResponse;
-
-            // handle long responses
-            if (parsedResponse.length > 4096) {
-                const chunks = Math.ceil(parsedResponse.length / 4096)
-                for (let i = 0; i < chunks; i++) {
-                    const index = 4096 * i
-                    await ctx.reply(parsedResponse.substring(index, index + 4096), {
-                        reply_to_message_id: ctx.msg.message_id,
-                        link_preview_options: {
-                            is_disabled: true
-                        },
-                    })
-                }
-            } else {
-                await ctx.reply(parsedResponse, {
+        // handle long responses
+        if (parsedResponse.length > 4096) {
+            const chunks = Math.ceil(parsedResponse.length / 4096)
+            for (let i = 0; i < chunks; i++) {
+                const index = 4096 * i
+                await ctx.reply(parsedResponse.substring(index, index + 4096), {
                     reply_to_message_id: ctx.msg.message_id,
                     link_preview_options: {
                         is_disabled: true
                     },
-                });
+                })
             }
-        } catch (error) {
-            console.error("Error calling OpenRouter API:", error);
-            const msg = typeof error === 'object' && error && 'description' in error ? error.description as string : "Sorry, there was an error processing your request."
-            await ctx.reply(msg, {
+        } else {
+            await ctx.reply(parsedResponse, {
                 reply_to_message_id: ctx.msg.message_id,
+                link_preview_options: {
+                    is_disabled: true
+                },
             });
         }
-    } else {
-        const msg = "You need to write more than that"
+    } catch (error) {
+        console.error("Error calling OpenRouter API:", error);
+        const msg = typeof error === 'object' && error && 'description' in error ? error.description as string : "Sorry, there was an error processing your request."
         await ctx.reply(msg, {
             reply_to_message_id: ctx.msg.message_id,
         });
